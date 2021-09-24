f8a8181815d4812c49cdb44256ca0ceb3b1af104
Weirer Mirjam <s44673@edu.campus02.at>
2021-09-24 12:07:27 +0200

    append cards

--- Bsp4/script/main.js
+++ Bsp4/script/main.js
@@ -7,3 +7,29 @@ const baseUrl = "http://nowaunoweb.azurewebsites.net/Content/Cards/";
 //     const number = document.getElementById("number");
 //     alert(`Farbe: ${color.value}, Number: ${number.value}`);
 // });
+let button = document.getElementsByClassName("button");
+button[0].addEventListener("click",function(){
+    let color = document.getElementById("color").value;
+    let number = document.getElementById("number").value;
+    let ul =  document.querySelector("#cards ul");
+    let li = document.createElement("li");
+    let img = document.createElement("img");
+    ul.appendChild(li);
+    if (color[0] == "r"){
+        const card = color + number;
+        img.src = `${baseUrl}${card}.png`;
+        li.appendChild(img);
+    }else if (color[0] == "g"){
+        const card = color + number;
+        img.src = `${baseUrl}${card}.png`;
+        li.appendChild(img);
+    }else if (color[0] == "b"){
+        const card = color + number;
+        img.src = `${baseUrl}${card}.png`;
+        li.appendChild(img);
+    }else if (color[0] == "y"){
+        const card = color + number;
+        img.src = `${baseUrl}${card}.png`;
+        li.appendChild(img);
+    }
+});
