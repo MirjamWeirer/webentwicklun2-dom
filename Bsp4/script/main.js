cd2bc9d8885b8750eb987ca3e25d33f58dd99e97
Weirer Mirjam <s44673@edu.campus02.at>
2021-09-30 08:45:02 +0200

    change

--- Bsp4/script/main.js
+++ Bsp4/script/main.js
@@ -2,7 +2,7 @@
 const baseUrl = "http://nowaunoweb.azurewebsites.net/Content/Cards/";
 
 // hier kommen ihre Skripte hin
-//  document.getElementsByName("button")[0].addEventListener("click", function(){
+//  document.getElementsByTagName("button")[0].addEventListener("click", function(){
 //     const color = document.getElementById("color");
 //     const number = document.getElementById("number");
 //     alert(`Farbe: ${color.value}, Number: ${number.value}`);
