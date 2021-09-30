1c11249cf35baac22b24c9f783d8416dd1251a93
Weirer Mirjam <s44673@edu.campus02.at>
2021-09-30 08:23:40 +0200

    card und color

--- Bsp4/script/main.js
+++ Bsp4/script/main.js
@@ -7,29 +7,54 @@ const baseUrl = "http://nowaunoweb.azurewebsites.net/Content/Cards/";
 //     const number = document.getElementById("number");
 //     alert(`Farbe: ${color.value}, Number: ${number.value}`);
 // });
-let button = document.getElementsByClassName("button");
-button[0].addEventListener("click",function(){
-    let color = document.getElementById("color").value;
-    let number = document.getElementById("number").value;
-    let ul =  document.querySelector("#cards ul");
-    let li = document.createElement("li");
-    let img = document.createElement("img");
-    ul.appendChild(li);
-    if (color[0] == "r"){
-        const card = color + number;
-        img.src = `${baseUrl}${card}.png`;
-        li.appendChild(img);
-    }else if (color[0] == "g"){
-        const card = color + number;
-        img.src = `${baseUrl}${card}.png`;
-        li.appendChild(img);
-    }else if (color[0] == "b"){
-        const card = color + number;
-        img.src = `${baseUrl}${card}.png`;
-        li.appendChild(img);
-    }else if (color[0] == "y"){
-        const card = color + number;
-        img.src = `${baseUrl}${card}.png`;
-        li.appendChild(img);
-    }
+document.getElementsByTagName("button")[0].addEventListener("click", function(){
+    const colorInput = document.getElementById("color");
+    const numberInput = document.getElementById("number");
+
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const card = makeCardFromInput(colorInput.value, numberInput.value);
+    img.src = `${baseUrl}${card}.png`;
+    li.appendChild(img);
+
+    document.querySelector("#cards ul").appendChild(li);
 });
+
+function makeCardFromInput(color, number){
+    let result;
+    switch (color){
+        case "Red":
+        case "red":
+        case "Rot":
+        case "rot":
+        case "r":
+            result = "r";
+            break;
+        
+        case "Green":
+        case "green":
+        case "Grün":
+        case "grün":
+        case "g":
+            result = "g";
+            break;
+
+        case "Blue":
+        case "blue":
+        case "Blau":
+        case "blau":
+        case "b":
+            result = "b";
+            break;
+        
+        case "Yellow":
+        case "yellow":
+        case "Gelb":
+        case "gelb":
+        case "y":
+            result = "y";
+            break;
+
+        }
+    return result + number;
+}
